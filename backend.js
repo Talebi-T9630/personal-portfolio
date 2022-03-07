1196d459caf6f163e0b48d4f7b9c5cebcc9ab2d9
Tarane <Talebit@student.douglascollege.ca>
2022-03-07 10:21:41 -0800

    initial commit

---
+++ backend.js
@@ -0,0 +1,24 @@
+const express= require('express');
+const app= express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const https = require("https");
+const { json } = require('body-parser');
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cors());
+
+
+
+app.get('/',(req,res)=>{
+
+res.send('I am working muhahaha');
+res.send("hello hello");
+});
+
+
+
+
+app.listen(5000,()=>{console.log("Listening on port 5000")});
